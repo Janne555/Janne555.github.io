1e20d463f0efce03e3a592e99e2b89b1ca3f16b9
Janne Lavila <Janne555@users.noreply.github.com>
2020-09-15 20:41:18 +0300

    update versions

--- js/components/nav-content.js
+++ js/components/nav-content.js
@@ -2,7 +2,7 @@ import { getTemplate } from '../utils.js'
 
 class NavBar extends HTMLElement {
   async connectedCallback() {
-    const template = await getTemplate('../../templates/nav-content.html?ver=3')
+    const template = await getTemplate('../../templates/nav-content.html?ver=4')
     this.innerHTML = template.innerHTML
   }
 }
--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-import './components/nav-content.js?ver=3'
+import './components/nav-content.js?ver=4'
 
 let vh = window.innerHeight * 0.01;
 document.documentElement.style.setProperty('--vh', `${vh}px`);
