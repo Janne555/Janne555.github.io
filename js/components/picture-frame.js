5a88149bd01d903ee0c0d2ad5b5eff8d8ee78980
Janne Lavila <Janne555@users.noreply.github.com>
2020-09-16 21:37:05 +0300

    doodling with background

--- js/components/picture-frame.js
+++ js/components/picture-frame.js
@@ -2,6 +2,19 @@ class PictureFrame extends HTMLElement {
 
   async connectedCallback() {
     const src = this.getAttribute("src")
+    const observer = new IntersectionObserver(event => {
+      if (event.some(entry => entry.isIntersecting)) {
+        this.classList.toggle("foo")
+        document.querySelector("#dynamic-style").innerHTML = `body { background: url(${src}); background-size: cover;}`
+      } else {
+        this.classList.toggle("foo")
+      }
+    }, {
+      root: document.root,
+      threshold: 1.0
+    })
+
+    observer.observe(this)
 
     this.innerHTML = `
       <style>
