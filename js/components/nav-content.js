c08fb8e0a5acca1c45663eec06b0776169075c50
Janne Lavila <Janne555@users.noreply.github.com>
2020-09-16 09:51:59 +0300

    add indicator for current page

--- js/components/nav-content.js
+++ js/components/nav-content.js
@@ -4,6 +4,11 @@ class NavBar extends HTMLElement {
   async connectedCallback() {
     const template = await getTemplate('../../templates/nav-content.html?ver=4')
     this.innerHTML = template.innerHTML
+
+    const currentpage = this.getAttribute("currentpage")
+    if (currentpage) {
+      this.querySelector(`#${currentpage}-item`).setAttribute('data-active', 'true')
+    }
   }
 }
 
