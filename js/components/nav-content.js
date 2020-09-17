728cee24ae50803ac5eb8850a4134f9ba22e0cc8
Janne Lavila <Janne555@users.noreply.github.com>
2020-09-17 16:12:59 +0300

    update versions

--- js/components/nav-content.js
+++ js/components/nav-content.js
@@ -1,8 +1,8 @@
-import { getTemplate } from '../utils.js?ver=8'
+import { getTemplate } from '../utils.js?ver=9'
 
 class NavBar extends HTMLElement {
   async connectedCallback() {
-    const template = await getTemplate('../../templates/nav-content.html?ver=8')
+    const template = await getTemplate('../../templates/nav-content.html?ver=9')
     this.innerHTML = template.innerHTML
 
     const currentpage = this.getAttribute("currentpage")
--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
-import './components/nav-content.js?ver=8'
-import './components/picture-frame.js?ver=8'
+import './components/nav-content.js?ver=9'
+import './components/picture-frame.js?ver=9'
 
 let vh = window.innerHeight * 0.01;
 document.documentElement.style.setProperty('--vh', `${vh}px`);
