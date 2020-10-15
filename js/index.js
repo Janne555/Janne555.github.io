846562d86eeb7d2cf61a640a1abb50480c6048a4
Janne Lavila <Janne555@users.noreply.github.com>
2020-10-16 00:52:51 +0300

    add content

--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@ import './components/picture-frame.js?ver=11'
 let vh = window.innerHeight * 0.01;
 document.documentElement.style.setProperty('--vh', `${vh}px`);
 
-// window.addEventListener('resize', () => {
-//   let vh = window.innerHeight * 0.01;
-//   document.documentElement.style.setProperty('--vh', `${vh}px`);
-// });
+window.addEventListener('resize', () => {
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
