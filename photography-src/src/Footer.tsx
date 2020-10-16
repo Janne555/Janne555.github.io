95d6ecf347838a4e33939cdff98a308cfeb1f22f
Janne Lavila <Janne555@users.noreply.github.com>
2020-10-16 22:16:37 +0300

    remove extra stuff

--- photography-src/src/Footer.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Footer: React.FC<{}> = ({ children }) => {
-  return (
-    <footer>{children}</footer>
-  )
-}
-
-export default Footer
--- photography-src/src/Header.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Header: React.FC<{}> = ({ children }) => {
-  return (
-    <header>{children}</header>
-  )
-}
-
-export default Header
